06931f39fbd3e1c732c55b16ffaec74201431f01
Michael Lemucchi <109193002+michaellemucchi@users.noreply.github.com>
2024-05-15 12:25:16 -0700

    executable front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import ChangePassword from './components/ChangePassword';
 import MealDetail from './components/MealDetail';
@@ -7,11 +7,11 @@ import MealDetail from './components/MealDetail';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/profile" component={Profile} />
-        <Route path="/change-password" component={ChangePassword} />
-        <Route path="/meals/:id" component={MealDetail} />
-      </Switch>
+      <Routes>
+        <Route path="/profile" element={Profile} />
+        <Route path="/change-password" element={ChangePassword} />
+        <Route path="/meals/:id" element={MealDetail} />
+      </Routes>
     </Router>
   );
 }
