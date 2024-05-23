1a974995cb37422e6068bd4c36ae1f245cf4f580
Michael Lemucchi <109193002+michaellemucchi@users.noreply.github.com>
2024-05-23 16:41:47 -0700

    filling in content for landing page v2

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import './LandingPage.css'; 
+import chart from '../images/chart.jpg';
 
 
 const LandingPage = () => {
   return (
     <div>
+
       <NavBar />
+
+      <div className="landing-container">
+        <div className="title-section">
+          <h1>Fitness-Nutrition Tracker</h1>
+          <p>This is your one-stop solution for tracking your workouts and nutrition!</p>
+        </div>
+        <div className="image-section">
+          <h1>Data Driven Tracking</h1>
+          <p>When storing your workouts in our tracker, you will be able to see your progress visually through 
+             motivating charts and graphs
+          </p>
+          <img src={chart} alt="Fitness Chart" />
+        </div>
+      </div>
+
     </div>
   );
 };
