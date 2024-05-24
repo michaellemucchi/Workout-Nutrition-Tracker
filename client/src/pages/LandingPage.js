291528614c877214de1f63bdd8acb2ac8982472e
Michael Lemucchi <109193002+michaellemucchi@users.noreply.github.com>
2024-05-23 17:54:34 -0700

    updated landing page v3

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -11,18 +11,39 @@ const LandingPage = () => {
       <NavBar />
 
       <div className="landing-container">
+
+        <div className="circle-large"></div> 
+        <div className="circle-small"></div>
+
         <div className="title-section">
           <h1>Fitness-Nutrition Tracker</h1>
           <p>This is your one-stop solution for tracking your workouts and nutrition!</p>
         </div>
-        <div className="image-section">
-          <h1>Data Driven Tracking</h1>
-          <p>When storing your workouts in our tracker, you will be able to see your progress visually through 
-             motivating charts and graphs
-          </p>
-          <img src={chart} alt="Fitness Chart" />
+
+        <div className="image-part">
+
+          <div className="image-description">
+            <h1>Data Driven Tracking</h1>
+            <p>When storing your workouts in our tracker, you will be able to see your progress visually through 
+              motivating charts and graphs
+            </p>
+          </div>
+
+          <div className="image-photo">
+            <img src={chart} alt="Fitness Chart" />
+          </div>
+
         </div>
+
       </div>
+
+      {/*add the feature card part */}
+      <div className="featureCards">
+      
+      </div>
+      
+      
+      {/*add footer (probably use component) */}
 
     </div>
   );
