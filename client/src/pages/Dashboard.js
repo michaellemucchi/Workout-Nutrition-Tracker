779f771b01bca27d6884757b9643ae7a520cf2df
Michael Lemucchi <109193002+michaellemucchi@users.noreply.github.com>
2024-05-27 00:31:04 -0700

    updated dashbord for relogin

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { useUser } from '../context/UserContext';
-import './Dashboard.css';
-import { Link } from 'react-router-dom';
+import './Dashboard.css'; 
+import Navbar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const Dashboard = () => {
     const { user } = useUser();
 
     if (!user) {
-        return <Link to="/login" className='please-login'>Please log in</Link>;
+        return (
+            <div>
+                <Navbar/>
+                <p className="dashboard-unauthorized">Please log in</p>
+                <Footer/>
+            </div>
+        );
     }
 
+    // Your dashboard content for logged-in users
     return (
-        <div>Welcome to your dashboard, {user.username}!</div>
+        <div className="dashboard">
+            <div>Welcome to your dashboard, {user.username}!</div>
+            {/* Additional dashboard components here */}
+        </div>
     );
 };
 
